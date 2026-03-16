02e61d5d23e67e96355441d0850cb619233baf00
Qs315490 <qs315490@outlook.com>
2026-03-16 17:41:38 +0800

    fonts: 修复 OTF 转换问题并优化并行处理

--- fonts/main.py
+++ fonts/main.py
@@ -1,6 +1,16 @@
 import os
-from multiprocessing import Pool, Manager
+from functools import partial
+from multiprocessing import Manager, Pool
+
 from fontTools.ttLib import TTFont
+from otf2ttf.cli import MAX_ERR, REVERSE_DIRECTION, otf_to_ttf
+from psutil import cpu_count
+
+otf_to_ttf = partial(
+    otf_to_ttf,
+    max_err=MAX_ERR,
+    reverse_direction=REVERSE_DIRECTION,
+)
 
 os.chdir(os.path.dirname(__file__))
 
@@ -10,9 +20,11 @@ output_dir = "./output"
 os.mkdir(input_dir) if not os.path.exists(input_dir) else None
 os.mkdir(output_dir) if not os.path.exists(output_dir) else None
 
-def ttf2woff2(file:str, fail_list):
+def ttf2woff2(file: str, fail_list: list[str]):
     try:
         font = TTFont(os.path.join(input_dir, file))
+        if font.sfntVersion == "OTTO":
+            otf_to_ttf(font)
     except Exception as e:
         print(e)
         fail_list.append(file)
@@ -26,10 +38,14 @@ def ttf2woff2(file:str, fail_list):
 
 
 def main():
-    pool = Pool(processes=4)
-    fail_list = Manager().list([])
+    pool = Pool(processes=cpu_count(logical=False))
+    fail_list = Manager().list()
     for file in os.listdir(input_dir):
-        pool.apply_async(ttf2woff2, args=(file, fail_list))
+        ext = os.path.splitext(file)[1].lower()
+        match ext:
+            case ".ttf" | ".otf":
+                pool.apply_async(ttf2woff2, args=(file, fail_list))
+
     pool.close()
     pool.join()
     print(f"{len(fail_list)} 个文件转换失败")
